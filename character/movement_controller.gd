c276a3a0e08d20e562d9161a4bd1bbf79353bc56
Steven Ireland <steven.ireland@protonmail.com>
2024-09-24 22:21:08 -0400

    actually almost working basically

--- character/movement_controller.gd
+++ character/movement_controller.gd
@@ -19,12 +19,15 @@ func _process(delta: float) -> void:
 	var left = -1 if Input.is_action_pressed("ui_left") else 1 if Input.is_action_pressed("ui_right") else 0
 	var forward = -1 if Input.is_action_pressed("ui_down") else 1 if Input.is_action_pressed("ui_up") else 0
 	var direction = Vector2(left, forward)
+	
+	$Skeleton3D.global_position = follow.global_position + Vector3(1, -1, 0);
+	$Skeleton3D.rotation.y = get_parent().get_node("SpringArm3D").rotation.y
 		
 	if Input.is_action_just_pressed("ui_accept"):
 		$AnimationPlayer.play("poses/run_jump")
 	if forward > 0:
-		$AnimationPlayer.play("poses/run")
+		$AnimationPlayer.play("poses/forward")
 	elif forward < 0:
 		$AnimationPlayer.play("poses/back_run")
-	#else:
-		#$Pose/AnimationPlayer.play("poses/idle")
+	else:
+		$AnimationPlayer.play("poses/idle")
